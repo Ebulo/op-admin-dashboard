993821300e4e2ddc2793f0cc3def6b782f5a9050
Bishant <bishant.nayak44@gmail.com>
2025-07-15 06:59:02 +0530

    tiny update

--- src/app/(admin)/(others-pages)/billing/page.tsx
+++ src/app/(admin)/(others-pages)/billing/page.tsx
@@ -1,51 +1,3 @@
-// "use client";
-
-// import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-// import BasicTableOne from "@/components/tables/BasicTableOne";
-// import React, { useEffect, useState, useCallback } from "react";
-// import { columns } from "./data";
-// import { getAdminBillings } from "@/api/billingApi";
-// import { Billing } from "@/types/billing";
-// import { RotateCw } from "lucide-react";
-// import Button from "@/components/ui/button/Button";
-
-// export default function BillingTables() {
-//   const [billings, setBillings] = useState<Billing[]>([]);
-//   const [loading, setLoading] = useState(false);
-
-//   const fetchBillings = useCallback(async () => {
-//     setLoading(true);
-//     const data = await getAdminBillings();
-//     setBillings(data);
-//     setLoading(false);
-//   }, []);
-
-//   useEffect(() => {
-//     fetchBillings();
-//   }, [fetchBillings]);
-
-//   return (
-//     <div>
-//       <PageBreadcrumb
-//         pageTitle="My Billings"
-//         pageActions={
-//           <div className="flex items-center gap-2">
-//             <Button
-//               onClick={fetchBillings}
-//               size="sm"
-//             >
-//               <RotateCw size={16} className={loading ? "animate-spin" : ""} />
-//               Refresh
-//             </Button>
-//           </div>
-
-//         }
-//       />
-//       <BasicTableOne data={billings} columns={columns(fetchBillings)} showSerialNumber={true} />
-//     </div>
-//   );
-// }
-
 "use client";
 
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
@@ -57,7 +9,7 @@ import { Billing } from "@/types/billing";
 import { RotateCw } from "lucide-react";
 import Button from "@/components/ui/button/Button";
 
-const STATUS_OPTIONS = ["PENDING", "SUBMITTED", "APPROVED", "PAID"];
+const STATUS_OPTIONS = ["ALL", "PENDING", "SUBMITTED", "APPROVED", "PAID"];
 
 export default function BillingTables() {
   const [billings, setBillings] = useState<Billing[]>([]);
@@ -167,7 +119,7 @@ export default function BillingTables() {
             >
               {STATUS_OPTIONS.map((status) => (
                 <option key={status} value={status}>
-                  {status === "ALL" ? "All Statuses" : status}
+                  {status === "ALL" ? "ALL Status" : status}
                 </option>
               ))}
             </select>
